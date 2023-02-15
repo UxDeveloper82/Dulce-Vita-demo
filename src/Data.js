21a14a311c1ea7136ce4e6d59511aee1c5917482
Martin Fernandez <Electronicsmf@aol.com>
2023-02-15 07:05:45 -0500

    adding shopping cart

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Small-Massage ",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/services1.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Full-Massage",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/services2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Tiny-Massage",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/services3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Full-Massage",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/services4.jpg",
+    },
+  ];
